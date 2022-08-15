95b483096a290d3ad9a9c68d9e30a93262229940
eswar <eswara.allu@gmail.com>
2022-08-15 14:07:43 -0500

    max number in the given Array.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {30, 90, 140, 139};
-        int size = array.length;
+        int[] arr = {10, 324, 45, 90, 9808};
+        int max = Arrays.stream(arr).max().getAsInt();
 
-        for(int i = 0; i<size; i++ ){
-            for(int j = i+1; j<size; j++){
-                if(array[i]>array[j]){
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
+        System.out.println("Largest in given array is " +max);
+
         }
-        System.out.println("Max Number: "+array[size-1]);
     }
-}
